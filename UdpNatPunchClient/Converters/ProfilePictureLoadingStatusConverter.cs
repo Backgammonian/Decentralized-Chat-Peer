805fec4576c6cf115257e7d1c578e6f5730fbf7b
Dexp <Dexp@DESKTOP-G7BK324>
2022-08-30 00:35:56 +0900

    update

---
+++ UdpNatPunchClient/Converters/ProfilePictureLoadingStatusConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using UdpNatPunchClient.Models;
+
+namespace Converters
+{
+    public class ProfilePictureLoadingStatusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (ProfilePictureLoadingStatusType)value;
+
+            switch (status)
+            {
+                case ProfilePictureLoadingStatusType.None:
+                    return string.Empty;
+
+                case ProfilePictureLoadingStatusType.Loading:
+                    return "Loading... 🔄";
+
+                case ProfilePictureLoadingStatusType.ErrorOccurred:
+                    return "Error ❎";
+
+                case ProfilePictureLoadingStatusType.Completed:
+                    return "Completed ✓";
+
+                default:
+                    return "---";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ UdpNatPunchClient/Extensions/ListBoxExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Specialized;
+
+namespace Extensions
+{
+    public static class ListBoxExtensions
+    {
+        public static readonly DependencyProperty AutoScrollToEndProperty =
+            DependencyProperty.RegisterAttached("AutoScrollToEnd", typeof(bool), typeof(ListBoxExtensions), new UIPropertyMetadata(OnAutoScrollToEndChanged));
+        private static readonly DependencyProperty AutoScrollToEndHandlerProperty =
+            DependencyProperty.RegisterAttached("AutoScrollToEndHandler", typeof(NotifyCollectionChangedEventHandler), typeof(ListBoxExtensions));
+
+        public static bool GetAutoScrollToEnd(DependencyObject obj) => (bool)obj.GetValue(AutoScrollToEndProperty);
+        public static void SetAutoScrollToEnd(DependencyObject obj, bool value) => obj.SetValue(AutoScrollToEndProperty, value);
+
+        private static void OnAutoScrollToEndChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+        {
+            var listBox = s as ListBox;
+
+            if (listBox == null)
+            {
+                return;
+            }
+
+            var source = (INotifyCollectionChanged)listBox.Items.SourceCollection;
+
+            if ((bool)e.NewValue)
+            {
+                NotifyCollectionChangedEventHandler scrollToEndHandler = delegate
+                {
+                    if (listBox.Items.Count <= 0)
+                    {
+                        return;
+                    }
+
+                    listBox.Items.MoveCurrentToLast();
+                    listBox.ScrollIntoView(listBox.Items.CurrentItem);
+                };
+
+                source.CollectionChanged += scrollToEndHandler;
+
+                listBox.SetValue(AutoScrollToEndHandlerProperty, scrollToEndHandler);
+            }
+            else
+            {
+                var handler = (NotifyCollectionChangedEventHandler)listBox.GetValue(AutoScrollToEndHandlerProperty);
+
+                source.CollectionChanged -= handler;
+            }
+        }
+    }
+}
